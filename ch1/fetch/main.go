a909c0645343b6979ee09c826cd3ab55fb007403
Glenn Oppegard <oppegard@gmail.com>
2017-11-05 09:21:18 -0700

    Exercise 1.8

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -12,10 +12,14 @@ import (
 	"net/http"
 	"os"
 	"io"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url,"http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
